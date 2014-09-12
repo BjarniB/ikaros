e7e07725e88a70a0d999ae55b37cc08fe1a7fb02
Asas Innab <adi10ain@student.lu.se>
2014-09-12 19:04:56 +0200

    Worked with RobotRecorder - Works to record and replay movements from all three servos. Added further recording and replaying mechanisms for torque and speed

--- Source/UserModules/RobotRecorder/RobotRecorder.h
+++ Source/UserModules/RobotRecorder/RobotRecorder.h
@@ -21,19 +21,46 @@ public:
     float *     input_array;
     int         input_array_size;
     
-    //float *     input_state;
-    //int         input_state_size;
+    float *     input_state;
+    int         input_state_size;
+    
+    float * 	input_load_array;
+    int 	input_load_array_size;
+
 
     float *     output_array;
     int         output_array_size;
+    
+     float *    output_diff_array;
+    int         output_diff_array_size;
+    
+    float *	output_load_array;
+    int 	output_load_array_size;
     
     
 
 
     // internal data storage
 
-    float *     internal_array;
-    int    internal_array_size;
+    float *     internal_array_serv1;
+    int    internal_array_size_serv1;
+    
+    float *     internal_array_serv2;
+    int    internal_array_size_serv2;
+    
+    float *     internal_array_serv3;
+    int    internal_array_size_serv3;
+    
+    
+    //load
+    float *     internal_load_serv1;
+    int    internal_load_size_serv1;
+    
+    float *     internal_load_serv2;
+    int    internal_load_size_serv2;
+    
+    float *     internal_load_serv3;
+    int    internal_load_size_serv3;
 
     // parameter values
 
